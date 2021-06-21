ca926e6a21319cac79d59253e2458130038d15bb
João Gomes <jolinharesgomes@gmail.com>
2021-06-21 18:55:56 +0100

    Add placeholder prop for InputField

--- resources/js/components/Notes/InputField.vue
+++ resources/js/components/Notes/InputField.vue
@@ -6,7 +6,7 @@
         type="text"
         :class="errorClass"
         class="py-2 px-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-md border"
-        placeholder="Write your thoughts..."
+        :placeholder="placeholder"
     />
 </template>
 
@@ -17,6 +17,12 @@ export default {
             type: String,
             default: '',
             required: true,
+        },
+        placeholder: {
+            type: String,
+            default() {
+                return 'Write your thoughts...'
+            }
         },
         hasError: {
             type: Boolean
